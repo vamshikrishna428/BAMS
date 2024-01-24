4e1373999d4e99095b34dcd3bcf8a3b6189cb2ba
vamshikrishna428 <vamshikrishnakr@gmail.com>
2024-01-24 17:50:28 +0530

    Hi

--- SCHOOLMANAGEMENTSYSTEM/src/test/java/FACEGENIE/SampleTest.java
+++ SCHOOLMANAGEMENTSYSTEM/src/test/java/FACEGENIE/SampleTest.java
@@ -17,68 +17,68 @@ import ObjectRepository.ManageLicensePage;
 public class SampleTest {
 	public static void main(String[] args) throws InterruptedException {
 
-		WebDriver driver=new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 
 		driver.get("https://facegenie-ams-school.web.app/");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
-		LogInPage loginpage=new LogInPage(driver);
+		LogInPage loginpage = new LogInPage(driver);
 
 		loginpage.EmaidTxField();
 		loginpage.PasswordTxField();
 		loginpage.LoginButton();
 
-		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(15));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 		wait.until(ExpectedConditions.urlMatches("https://facegenie-ams-school.web.app/dashboard/home"));
 
-
-		JavascriptExecutor j = (JavascriptExecutor)driver;
+		JavascriptExecutor j = (JavascriptExecutor) driver;
 		WebElement click = driver.findElement(By.xpath("//span[contains(text(),'Manage Licenses')]"));
 		j.executeScript("arguments[0].click();", click);
 
-		WebElement selectbus = driver.findElement(By.xpath("//div[@class='MuiFormControl-root MuiFormControl-fullWidth css-1mejovb']"));
+		WebElement selectbus = driver
+				.findElement(By.xpath("//div[@class='MuiFormControl-root MuiFormControl-fullWidth css-1mejovb']"));
 		selectbus.click();
 
-		ManageLicensePage page=new ManageLicensePage(driver);
+		ManageLicensePage page = new ManageLicensePage(driver);
 		page.DropDown();
 
 		WebElement EID = driver.findElement(By.xpath("//button[contains(text(),'yash')]"));
 		Thread.sleep(2000);
-		j.executeScript("arguments[0].scrollIntoView(true);",EID);
+		j.executeScript("arguments[0].scrollIntoView(true);", EID);
 		Thread.sleep(2000);
 		j.executeScript("arguments[0].click();", EID);
 
 		driver.findElement(By.xpath("//p[contains(text(),'Edit Details')]")).click();
 
-		WebElement empid = driver.findElement(By.xpath("//div[@class='MuiGrid-root css-1rlxlch']/descendant::input[@name='empId']"));
-		j.executeScript("arguments[0].value = '';",empid);
+		WebElement empid = driver
+				.findElement(By.xpath("//div[@class='MuiGrid-root css-1rlxlch']/descendant::input[@name='empId']"));
+		j.executeScript("arguments[0].value = '';", empid);
 		Thread.sleep(2000);
-		j.executeScript("arguments[0].value = 'Yash123';",empid);
+		j.executeScript("arguments[0].value = 'Yash123';", empid);
 
-		WebElement empname = driver.findElement(By.xpath("//div[@class='MuiGrid-root css-1rlxlch']/descendant::input[@name='firstName']"));
-		j.executeScript("arguments[0].value = '';",empname);
+		WebElement empname = driver
+				.findElement(By.xpath("//div[@class='MuiGrid-root css-1rlxlch']/descendant::input[@name='firstName']"));
+		j.executeScript("arguments[0].value = '';", empname);
 		Thread.sleep(2000);
-		j.executeScript("arguments[0].value = 'YashChopra';",empname);
+		j.executeScript("arguments[0].value = 'YashChopra';", empname);
 		Thread.sleep(2000);
 
-		WebElement updatelicense = driver.findElement(By.xpath("//div[@class='MuiGrid-root jss32 css-rfnosa']/descendant::p[contains(text(),'Update License')]"));
-		if(updatelicense.isDisplayed())
-		{
-			j.executeScript("arguments[0].scrollIntoView(true);",updatelicense);
+		WebElement updatelicense = driver.findElement(By.xpath(
+				"//div[@class='MuiGrid-root jss32 css-rfnosa']/descendant::p[contains(text(),'Update License')]"));
+		if (updatelicense.isDisplayed()) {
+			j.executeScript("arguments[0].scrollIntoView(true);", updatelicense);
 			Thread.sleep(2000);
 			updatelicense.click();
-		}
-		else
-		{
-			driver.findElement(By.xpath("//div[@class='MuiGrid-root jss42 css-rfnosa']/descendant::p[contains(text(),'Back')]")).click();
+		} else {
+			driver.findElement(
+					By.xpath("//div[@class='MuiGrid-root jss42 css-rfnosa']/descendant::p[contains(text(),'Back')]"))
+					.click();
 		}
 
-		LogOutPage lop=new LogOutPage(driver);
+		LogOutPage lop = new LogOutPage(driver);
 		lop.LogOutButton();
 		lop.OkButton();
-
-
 
 	}
 
